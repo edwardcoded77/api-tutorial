a23f03060fc661c9e16d01449bf577e57c0703d2
edwardcoded77 <eolagunju25@email.com>
2026-08-27 02:47:56 -0400

    json in js closes #3

---
+++ scripts/description.js
@@ -0,0 +1,11 @@
+import CatData from "../pet-cats.json";
+console.log("Hello World!");
+
+// The first cat
+console.log("The " + CatData[0].name + "'s  shredding level is " + CatData[0].shedding + ".");
+
+// The second cat 
+console.log("The " + CatData[1].name + "'s  shredding level is " + CatData[0].shedding + ".");
+
+// The third cat 
+console.log("The " + CatData[2].name + "'s  shredding level is " + CatData[0].shedding + ".");
