537ebdc5e66435922cbdf53a057b13d7317cccba
edwardcoded77 <eolagunju25@email.com>
2026-08-28 01:48:19 -0400

    More cards created closes #4

--- scripts/description.js
+++ scripts/description.js
@@ -1,11 +1,66 @@
-import CatData from "../my-cats.json";
-console.log("Hello World!");
+import pets from "../data.json";
 
-// The first cat
-console.log("The " + CatData[0].name + "'s  shredding level is " + CatData[0].shedding + ".");
+function introduce(breed, origin){
+    console.log("The " + breed + " is a cat from " + origin + ".");
+}
 
-// The second cat 
-console.log("The " + CatData[1].name + "'s  shredding level is " + CatData[0].shedding + ".");
+introduce("Bengal Cats", "USA");
+introduce("American Curl", "California, USA");
+introduce("Bombay", "Kentucky, USA");
+introduce("British", "Great Britain");
+introduce("Burmilla", "England");
 
-// The third cat 
-console.log("The " + CatData[2].name + "'s  shredding level is " + petCat[0].shedding + ".");
+
+
+
+
+
+
+
+
+
+
+
+
+// function sayLength(size) {
+//   console.log("This cat measures " + size + ".");
+// }
+
+// function describe(pet) {
+//   console.log(pet.name + " comes from " + pet.origin + ".");
+// }
+
+// sayLength("12 to 16 inches");
+// sayLength("15 to 20 inches");
+// sayLength("30 to 40 inches");
+
+// describe(pets[0]);
+// describe(pets[1]);
+// describe(pets[2]);
+// // describe();
+
+
+// console.log(pets[0]);
+// console.log(pets[0].name + " " + pets[0].grooming);
+
+
+
+
+
+
+
+// import CatData from "../my-cats.json";
+
+// console.log("Hello World!");
+
+// // The first cat
+// console.log( "The " + CatData[0].name + "'s shedding level is " + CatData[0].shedding + "."
+// );
+
+// // The second cat
+// console.log( "The " + CatData[1].name + "'s shedding level is " + CatData[1].shedding + "."
+// );
+
+// // The third cat
+// console.log( "The " + CatData[2].name + "'s shedding level is " + CatData[2].shedding + "."
+// );
