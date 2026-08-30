9612ad64d7955bb59391a78a57cabca7703ca82b
edwardcoded77 <eolagunju25@email.com>
2026-08-30 19:58:56 -0400

    day-4 link created

--- day-4.js
+++ day-4.js
@@ -6,9 +6,9 @@ function summarize(pet) {
   return pet.name + " comes from " + pet.origin + " and weighs up to " + pet.max_weight + " pounds.";
 }
 
-function summarize(pet) {
-    console.log(pet.name + " is a " + pet.breed);
-}
+// function summarize(pet) {
+//     console.log(pet.name + " is a " + pet.breed);
+// }
 
 button.addEventListener("click", function () {
   output.textContent = summarize(pets[3]);
